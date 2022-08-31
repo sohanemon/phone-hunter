0ecef165f8506ce1899580588c32b6e55f53c9be
sohanemon <m.sohanemon@gmail.com>
2022-08-31 14:01:48 +0600

    responsive done

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ document.body.addEventListener("click", () => {
   $("modal").innerHTML = "";
 });
 dataFetch("Our recent collections");
+
+window.onscroll = () => {
+  console.log("sro");
+
+  /* all may return heigt of navbar element
+  console.log($("navbar").scrollHeight);
+  console.log($("navbar").clientHeight);
+  console.log($("navbar").offsetHeight); */
+
+  const navClass = $("navbar").classList;
+  if ($("navbar").offsetTop >= 100) {
+    navClass.remove("w-3/4");
+    navClass.add("w-full");
+    console.log("more");
+  } else {
+    console.log("less");
+    navClass.remove("w-full");
+    navClass.add("w-3/4");
+  }
+};
